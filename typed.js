1d4339e764a14871fae615aa960896647e101046
sunil-kumar-sahoo <sunilkumarsahoo447@gmail.com>
2024-12-03 21:45:40 +0530

    first commit

---
+++ typed.js
@@ -0,0 +1,15 @@
+// Initialize Typed.js animation
+let typed = new Typed('#typed-output', {
+    strings: [
+        "Fullstack Developer",
+        "UI/UX Enthusiast",
+        "Web Designer"
+        
+    ],
+    typeSpeed: 80, // Typing speed in milliseconds
+    backSpeed: 40, // Backspacing speed in milliseconds
+    startDelay: 1000, // Delay before typing starts in milliseconds
+    backDelay: 2000, // Delay before backspacing starts in milliseconds
+    loop: true, // Loop the animation
+    showCursor: false // Hide the typing cursor
+});
